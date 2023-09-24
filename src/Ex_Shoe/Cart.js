d15fdf03a071abc9ed07c08ab8031f92c369bdce
DangPhamQuocBao <quocbao0165@gmail.com>
2023-09-24 16:20:41 +0700

    xong bai tap

---
+++ src/Ex_Shoe/Cart.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+
+export default class Cart extends Component {
+  renderTbody = () => {
+    let { cart, handleRemove, handleChangeQuantity } = this.props;
+    return cart.map((item) => {
+      return (
+        <tr>
+          <td>${item.name}</td>
+          <td>${item.price}</td>
+          <td>
+            <img width={100} src={item.image} alt="" />
+          </td>
+          <td>
+            <button
+              onClick={() => {
+                handleChangeQuantity(item.id, -1);
+              }}
+              className="btn btn-danger"
+            >
+              -
+            </button>
+            <strong>{item.soLuong}</strong>
+            <button
+              onClick={() => {
+                handleChangeQuantity(item.id, +1);
+              }}
+              className="btn btn-dark"
+            >
+              +
+            </button>
+          </td>
+          <td>
+            <button
+              onClick={() => {
+                handleRemove(item.id);
+              }}
+              className="btn btn-danger"
+            >
+              Delete
+            </button>
+          </td>
+        </tr>
+      );
+    });
+  };
+
+  render() {
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Image</th>
+            <th>Quantity</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>{this.renderTbody()}</tbody>
+      </table>
+    );
+  }
+}
---
+++ src/Ex_Shoe/Ex_Shoe.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import Cart from "./Cart";
+import ListShoe from "./ListShoe";
+import DetailShoe from "./DetailShoe";
+import { shoeArr } from "./data";
+import { message } from "antd";
+
+export default class Ex_Shoe extends Component {
+  state = {
+    shoeArr: shoeArr,
+    detail: shoeArr[3],
+    cart: [],
+  };
+  handleAdd = (shoe) => {
+    let cloneCart = [...this.state.cart];
+    var index = cloneCart.findIndex((item) => {
+      return item.id == shoe.id;
+    });
+    if (index == -1) {
+      let newShoe = { ...shoe, soLuong: 1 };
+      cloneCart.push(newShoe);
+    } else {
+      cloneCart[index].soLuong++;
+    }
+    this.setState({
+      cart: cloneCart,
+    });
+  };
+  handleRemove = (shoeId) => {
+    let cloneCart = [...this.state.cart];
+    let index = cloneCart.findIndex((item) => item.id == shoeId);
+    cloneCart.splice(index, 1);
+    this.setState({ cart: cloneCart });
+    message.success("Xoá thành công");
+  };
+  handleViewDetail = (shoe) => {
+    this.setState({ detail: shoe });
+  };
+  handleChangeQuantity = (id, option) => {
+    let cloneCart = [...this.state.cart];
+    let index = cloneCart.findIndex((item) => item.id === id);
+    cloneCart[index].soLuong = cloneCart[index].soLuong + option;
+    cloneCart[index].soLuong == 0 && cloneCart.splice(index, 1);
+    this.setState({ cart: cloneCart });
+  };
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <div className="col-7">
+            <Cart handleChangeQuantity={this.handleChangeQuantity} handleRemove={this.handleRemove} cart={this.state.cart} />
+          </div>
+          <div className="col-5">
+            <ListShoe handleViewDetail={this.handleViewDetail} handleBuy={this.handleAdd} list={this.state.shoeArr} />
+          </div>
+        </div>
+        <DetailShoe detail={this.state.detail} />
+      </div>
+    );
+  }
+}
+
